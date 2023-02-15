e18ebab57d5adb7955288ccc1f28e950d2f2707b
JoseUmbria <125471796+JoseUmbria@users.noreply.github.com>
2023-02-15 14:59:58 +0100

    Add files via upload

---
+++ play/articles/home.php
@@ -0,0 +1,154 @@
+
+              <div class="title-section"><i class="enjoy"></i>
+                <h2> Enjoy</h2>
+              </div>
+              <!-- Swiper -->
+              <div class="swiper-container myTips">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Bruno Mars - Thats what I like</h3><img src="assets/images/stream/bruno-mars.jpg">
+                    </a></div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+              </div>
+            </section>
+
+            <section class="ready-section">
+              <div class="title-section"><i class="ready"></i>
+                <h2> Get fit</h2>
+              </div>
+              <div class="swiper-container myApps">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>BodyMaster XX</h3><img src="assets/images/apps/fitness.png">
+                    </a></div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+              </div>
+
+
+              <div class="swiper-container myRecipes">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip Churro Edible Cookie Dough Dip</h3><img
+                        src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip Churro Edible Cookie Dough Dip</h3><img
+                        src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, laboriosam mollitia ullam,
+                        autem tempore optio ad quae facere repellendus error neque veritatis laudantium ipsa. Totam
+                        facere rem exercitationem amet ipsum.</p>
+                      <h3>Churro Edible Cookie Dough Dip</h3><img src="assets/images/recipes/01.jpg">
+                    </a></div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+              </div>
+  
