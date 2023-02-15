9575f7477f530ac44855fca1c9529f7b482f9ede
JoseUmbria <125471796+JoseUmbria@users.noreply.github.com>
2023-02-15 15:03:21 +0100

    Add files via upload

---
+++ play/home.php
@@ -0,0 +1,68 @@
+
+      <div class="container">
+        <!--Here starts the home page-->
+        <div class="content">
+          <div class="swiper-container Slider">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="caption">
+                  <h2>Fortnite</h2>
+                  <p>Battle Royale</p><span class="tag">Games</span><a href=""><button
+                      class="btn color-btn blue">Open</button></a>
+                </div>
+                <picture>
+                  <source media="(max-width: 768px)" srcset="assets/images/slider/fortnite-small.jpg">
+                  <source media="(min-width: 769px)" srcset="assets/images/slider/fortnite.jpg">
+                  <img src="assets/images/slider/fortnite.jpg" alt="">
+                </picture>
+                
+              </div>
+              <div class="swiper-slide">
+                <div class="caption">
+                  <h2>Fitness</h2>
+                  <p>Get a healthy body</p><span class="tag">Be ready</span><a href=""><button
+                      class="btn color-btn green">Open</button></a>
+                </div> <picture>
+                  <source media="(max-width: 768px)" srcset="assets/images/slider/lifestyle-small.jpg">
+                  <source media="(min-width: 769px)" srcset="assets/images/slider/lifestyle.jpg">
+                  <img src="assets/images/slider/lifestyle.jpg" alt="">
+                </picture>
+              </div>
+              <div class="swiper-slide">
+                <div class="caption">
+                  <h2>Roblox</h2>
+                  <p>The "It" game</p><span class="tag">Games</span><a href=""><button
+                      class="btn color-btn blue">Open</button></a>
+                </div> <picture>
+                  <source media="(max-width: 768px)" srcset="assets/images/slider/roblox-small.jpg">
+                  <source media="(min-width: 769px)" srcset="assets/images/slider/roblox.jpg">
+                  <img src="assets/images/slider/roblox.jpg" alt="">
+                </picture>
+              </div>
+              <div class="swiper-slide">
+                <div class="caption">
+                  <h2>Bruno Mars</h2>
+                  <p>Music videos</p><span class="tag">Stream</span><a href=""><button
+                      class="btn color-btn purple">Open</button></a>
+                </div><picture>
+                  <source media="(max-width: 768px)" srcset="assets/images/slider/music-small.jpg">
+                  <source media="(min-width: 769px)" srcset="assets/images/slider/music.jpg">
+                  <img src="assets/images/slider/music.jpg" alt="">
+                </picture>
+              </div>
+              <div class="swiper-slide">
+                <div class="caption">
+                  <h2>Recipes</h2>
+                  <p>Delicious food for every mood</p><span class="tag">Be ready</span><a href=""><button
+                      class="btn color-btn green">Open</button></a>
+                </div><picture>
+                  <source media="(max-width: 768px)" srcset="assets/images/slider/recipes-small.jpg">
+                  <source media="(min-width: 769px)" srcset="assets/images/slider/recipes.jpg">
+                  <img src="assets/images/slider/recipes.jpg" alt="">
+                </picture>
+              </div>
+            </div>
+            <!-- Add Scrollbar -->
+            <div class="swiper-scrollbar"></div>
+          </div>
+      
