435e0641d6e4aa67eb8f4284d6e18f39e7989c34
JoseUmbria <125471796+JoseUmbria@users.noreply.github.com>
2023-02-15 15:01:18 +0100

    Add files via upload

---
+++ play/games/home.php
@@ -0,0 +1,61 @@
+ 
+              <div class="title-section"><i class="play"></i>
+                <h2> Play</h2>
+              </div>
+              <!-- Swiper -->
+              <div class="swiper-container myApps">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                  <div class="swiper-slide"><a href=""><img src="assets/images/apps/games.png"></a></div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+              </div>
+
+              <!-- Swiper -->
+              <div class="swiper-container myTips">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                  <div class="swiper-slide"><a href="">
+                      <h3>Best games for Android or iPhone</h3><img src="assets/images/apps/tips.png">
+                    </a></div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+              </div>
+
+  
